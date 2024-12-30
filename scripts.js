0fa8a5478f75f652c0ea3c9510559c3962f9a74e
ninisay30 <s72050@ocean.umt.edu.my>
2024-12-30 21:19:20 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+/*!
+ * FutureTech Solutions - Custom Scripts
+ */
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+// Basic form validation
+document.querySelector('form').addEventListener('submit', function (e) {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name || !email || !message) {
+        e.preventDefault();
+        alert('Please fill out all fields.');
+    }
+});
